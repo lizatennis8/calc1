bfac5d2033286d3ae32dd7e2255738274011e53e
Liza Tennis and Talia Haller <talia@>
2018-08-08 11:59:28 -0700

    add global tags for 4 function calc

--- calculator/js/scripts.js
+++ calculator/js/scripts.js
@@ -18,11 +18,33 @@ var divide = function(number1, number2){
 
 // Everything below this line is user interface (front-end) logic:
 
-$(document).ready(function() {
-	$("form#add").submit(function(event) {
+$(document).ready(function(){
+	$("form#math").submit(function(event) {
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-		alert(add(number1, number2));
+
+		var number1 = parseFloat($("#math1").val());
+		var number2 = parseFloat($("#math2").val());
+
+		$("#add").click(function(){
+			var result = add(number1, number2);
+			$("#output").text(result);
+	  });
+
+		$("#subtract").click(function(){
+      var result = subtract(number1, number2);
+      $("#output").text(result);
+    });
+
+    $("#multiply").click(function(){
+      var result = multiply(number1, number2);
+      $("#output").text(result);
+    });
+
+    $("#divide").click(function(){
+      var result = divide(number1, number2);
+      $("#output").text(result);
+    });
+
 	});
+
 });
